a39c1b4b4322ea68af74f475e1f7998aa19be8ad
santiago suarez <santiago.su.mo@gmail.com>
2019-10-26 16:01:55 +0200

    feat: Add basic stdin/stdout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Initial commit');
+process.stdin.setEncoding('utf8');
+
+let radarImage: string  = '';
+
+process.stdin.on('readable', () => {
+  let chunk;
+  while ((chunk = process.stdin.read()) !== null) {
+    radarImage += chunk;
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write(radarImage);
+});
