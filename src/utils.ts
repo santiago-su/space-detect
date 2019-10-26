c5df373a515e23f8df0675377c9e1f3cce011f3e
santiago suarez <santiago.su.mo@gmail.com>
2019-10-26 17:02:18 +0200

    feat: Add input parser

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const formatInput = (input:string) => {
+  const lines = input.trim().split('\n');
+  const nodes = lines.length;
+  const depth = lines[0].length;
+  const matrix = lines.map(line => [line]);
+  return { matrix, nodes, depth };
+};
